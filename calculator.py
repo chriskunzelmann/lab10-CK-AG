82f1fc2cc00c687a9f7edb657869cfb009700995
angie-garcia813 <garciaangie653@gmail.com>
2025-07-30 13:54:18 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,22 +1,23 @@
 import math
-def add(a, b): 
-    return a+b
 
-def sub(a,b):
-    return a-b
+def add(a,b): return a+b
 
-def mul(a,b):
-    return a*b
+def sub(a,b): return a-b
+
+def mul(a,b): return a*b
 
 def div(a,b):
     if a == 0:
-        raise ZeroDivisionError("Cannot divide by zero")
-    return b/a
+        raise ZeroDivisionError
+        print("Cannot divide a zero")
+    else:
+        return b/a
 
 def log(a,b):
-    if a==1 or a<=0 or b<=0:
-        raise ValueError("Inputs must be positive and base cannot be 1")
-    return math.log(b,a)
+    if a<=0 or b<= 0:
+        raise ValueError
+        print("ValueError")
+    else:
+        math.log(a,b)
 
-def exp(a,b):
-    return a**b
+def exp(a,b): return pow(a,b)
